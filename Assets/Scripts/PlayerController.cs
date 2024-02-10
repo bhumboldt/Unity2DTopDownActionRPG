9b5dcd54b6165cc2fcd16dbe2c76607c0dc69bed
Brandon Humboldt <brhumboldt@gmail.com>
2024-02-09 20:37:05 -0600

    player run and idle animations, flipping with mouse position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5.0f;
     private Vector2 _movement;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     
     void Awake()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     public void FixedUpdate()
     {
         this.Move();
+        this.FlipPlayerPosition();
     }
 
     void Update()
@@ -27,6 +32,8 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
+        _animator.SetFloat("MoveX", _movement.x);
+        _animator.SetFloat("MoveY", _movement.y);
     }
 
     private void Move()
@@ -34,5 +41,21 @@ public class PlayerController : MonoBehaviour
         var currPosition = _rigidbody.position;
         var newPosition = currPosition + _movement * (moveSpeed * Time.fixedDeltaTime);
         _rigidbody.MovePosition(newPosition);
+    }
+
+    private void FlipPlayerPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 playerPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (mousePosition.x < playerPosition.x)
+        {
+            _spriteRenderer.flipX = true;
+            // transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+            // transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
