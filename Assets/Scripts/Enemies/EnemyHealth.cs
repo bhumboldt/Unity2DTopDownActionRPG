49c3297b7d15960375d638ea3d69a77dd137d509
Brandon Humboldt <brhumboldt@gmail.com>
2024-02-10 19:47:30 -0600

    refactor scripts, make enemies take damage

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 3;
+    
+    private int currentHealth;
+    
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+        Debug.Log(currentHealth);
+        
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/DamageSource.cs
+++ Assets/Scripts/Player/DamageSource.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
+    [SerializeField] private int damage = 1;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<EnemyAI>())
+        var enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
         {
+            enemyHealth.TakeDamage(damage);
             Debug.Log("Enemy hit!");
         }
     }
