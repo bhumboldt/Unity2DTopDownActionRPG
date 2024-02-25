2e4e76ac1163bb06cf73de0438878c529745dd3c
Brandon Humboldt <brhumboldt@gmail.com>
2024-02-25 13:47:46 -0600

    spawn pop up on pickups

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Pickup : MonoBehaviour
 {
     [SerializeField] private float pickupDistance = 5f;
     [SerializeField] private float accelerationRate = 0.3f;
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
     
     private Vector3 moveDirection = Vector3.zero;
     private Rigidbody2D _rigidbody;
@@ -15,6 +19,11 @@ public class Pickup : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(AnimCurveSpawnRoutine());
     }
 
     private void Update()
@@ -44,6 +53,27 @@ public class Pickup : MonoBehaviour
         if (other.gameObject.GetComponent<PlayerController>())
         {
             Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine()
+    {
+        Vector2 startPoint = transform.position;
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+        Vector2 endPoint = new Vector2(randomX, randomY);
+        
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+            yield return null;
         }
     }
 }
