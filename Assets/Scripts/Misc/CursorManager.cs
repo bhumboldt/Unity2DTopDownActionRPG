bf1d5b6fd62da0cb627c5ab7dee5881b37958321
Brandon Humboldt <brhumboldt@gmail.com>
2024-02-25 14:00:22 -0600

    cursor

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+        if (Application.isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Input.mousePosition;
+        image.rectTransform.position = cursorPos;
+
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        else
+        {
+            Cursor.visible = false;
+        }
+    }
+}
